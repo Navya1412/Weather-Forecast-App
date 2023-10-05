d18bc61ea9421be7def7a7e22e6628159a6eb943
Navya <navyavinodlatha@gmail.com>
2023-10-06 00:30:23 +1100

    edits

--- script.js
+++ script.js
@@ -1,11 +1,59 @@
+// const apiKey = 'e7f2a44070f4f734b1ba8145c077a931';
+
+// async function getWeather() {
+//   const postCode = document.getElementById('zipCodeInput').value;
+
+//   if (postCode) {
+//     try {
+//       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${postCode}&appid=${apiKey}&units=metric`);
+//       const data = await response.json();
+
+//       if (response.ok) {
+//         displayWeather(data);
+//       } else {
+//         throw new Error(data.message);
+//       }
+//     } catch (error) {
+//       console.error('Error fetching weather:', error.message);
+//       displayError('Error fetching weather. Please try again.');
+//     }
+//   } else {
+//     displayError('Please enter a valid zip code.');
+//   }
+// }
+
+// function displayWeather(data) {
+//   const weatherResult = document.getElementById('weatherResult');
+
+//   const description = data.weather[0].description;
+//   const temperature = data.main.temp;
+//   const cityName = data.name;
+//   const humid = data.main.humidity;
+//   const visible = data.visibility;
+//   const speed = data.speed;
+
+//   weatherResult.innerHTML = `<p>Weather in ${cityName}: ${description}</p>
+//                             <p>Temperature: ${temperature}°C</p>
+//                             <p> Humidity: ${humid} </p>
+//                             <p> Visibility: ${visible} </p>
+//                             <p> Wind Speed: ${speed} </p>`;
+// }
+
+// function displayError(message) {
+//   const weatherResult = document.getElementById('weatherResult');
+//   weatherResult.innerHTML = `<p style="color: red;">${message}</p>`;
+// }
+
+
+
 const apiKey = 'e7f2a44070f4f734b1ba8145c077a931';
 
 async function getWeather() {
-  const zipCode = document.getElementById('zipCodeInput').value;
+  const postcode = document.getElementById('postcodeInput').value;
 
-  if (zipCode) {
+  if (postcode) {
     try {
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}&units=metric`);
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${postcode}&appid=${apiKey}&units=metric`);
       const data = await response.json();
 
       if (response.ok) {
@@ -18,7 +66,7 @@ async function getWeather() {
       displayError('Error fetching weather. Please try again.');
     }
   } else {
-    displayError('Please enter a valid zip code.');
+    displayError('Please enter a valid postcode.');
   }
 }
 
@@ -28,15 +76,9 @@ function displayWeather(data) {
   const description = data.weather[0].description;
   const temperature = data.main.temp;
   const cityName = data.name;
-  const humid = data.main.humidity;
-  const visible = data.visibility;
-  const speed = data.speed;
 
   weatherResult.innerHTML = `<p>Weather in ${cityName}: ${description}</p>
-                            <p>Temperature: ${temperature}°C</p>
-                            <p> Humidity: ${humid} </p>
-                            <p> Visibility: ${visible} </p>
-                            <p> Wind Speed: ${speed} </p>`;
+                            <p>Temperature: ${temperature}°C</p>`;
 }
 
 function displayError(message) {
